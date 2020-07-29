389e2c380624ae21179e4507fa183d12b713d049
Harmonised <harmonised7@gmail.com>
2020-07-29 12:09:08 +0300

    Error message made a bit more clear

--- src/main/java/harmonised/pmmo/util/XP.java
+++ src/main/java/harmonised/pmmo/util/XP.java
@@ -1086,7 +1086,7 @@ public class XP
 
 		if( !(player instanceof ServerPlayerEntity) )
 		{
-			LogHandler.LOGGER.error( "CLIENT PLAYER XP AWARD ATTEMPTED! THIS SHOULD NOT HAPPEN! SOURCE: " + sourceName + ", SKILL: " + skill.name() + ", AMOUNT: " + amount );
+			LogHandler.LOGGER.error( "NOT ServerPlayerEntity PLAYER XP AWARD ATTEMPTED! THIS SHOULD NOT HAPPEN! SOURCE: " + sourceName + ", SKILL: " + skill.name() + ", AMOUNT: " + amount );
 			return;
 		}
 
