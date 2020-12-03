dc8f9d553846dd3befabafe94e8c5a6681c48c3a
Harmonised <harmonised7@gmail.com>
2020-12-03 16:43:44 +0000

    Pmmo Star FTBQ~

--- src/main/java/harmonised/pmmo/events/RegisterHandler.java
+++ src/main/java/harmonised/pmmo/events/RegisterHandler.java
@@ -3,12 +3,13 @@ package harmonised.pmmo.events;
 import com.feed_the_beast.ftbquests.quest.task.TaskType;
 import com.feed_the_beast.mods.ftbguilibrary.icon.Icon;
 import harmonised.pmmo.ftb_quests.SkillTask;
+import harmonised.pmmo.util.Reference;
 import net.minecraftforge.event.RegistryEvent;
 
 public class RegisterHandler
 {
     public static void handleFTBQRegistry(RegistryEvent.Register<TaskType> event)
     {
-        event.getRegistry().register( SkillTask.SKILL = new TaskType( SkillTask::new ).setRegistryName( "skill" ).setIcon( Icon.getIcon("minecraft:item/wooden_pickaxe") ) );
+        event.getRegistry().register( SkillTask.SKILL = new TaskType( SkillTask::new ).setRegistryName( "skill" ).setIcon( Icon.getIcon( Reference.MOD_ID + ":textures/gui/star.png" ) ) );
     }
 }
