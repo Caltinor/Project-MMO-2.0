a1758e9ec078d8484458c0a2f72388085afc513c
Harmonised <harmonised7@gmail.com>
2021-01-21 14:19:14 +0000

    Skill.COMBAT instead of hardcode

--- src/main/java/harmonised/pmmo/events/DamageHandler.java
+++ src/main/java/harmonised/pmmo/events/DamageHandler.java
@@ -154,10 +154,10 @@ public class DamageHandler
                     Map<String, Double> weaponReq = XP.getJsonMap( resLoc, JType.REQ_WEAPON );
                     String skill = event.getSource().damageType.equals( "arrow" ) ? Skill.ARCHERY.toString() : Skill.COMBAT.toString();
                     String itemSpecificSkill = AutoValues.getItemSpecificSkill( itemStack.getItem().getRegistryName().toString() );
-                    if( skill.equals( "combat" ) )
+                    if( skill.equals( Skill.COMBAT.toString() ) )
                         skill = itemSpecificSkill;
                     if( Config.getConfig( "autoGenerateValuesEnabled" ) != 0 && Config.getConfig( "autoGenerateWeaponReqDynamicallyEnabled" ) != 0 )
-                        weaponReq.put( skill, weaponReq.getOrDefault( skill.toString(), AutoValues.getWeaponReqFromStack( itemStack ) ) );
+                        weaponReq.put( skill, weaponReq.getOrDefault( skill, AutoValues.getWeaponReqFromStack( itemStack ) ) );
                     int weaponGap = XP.getSkillReqGap( player, weaponReq );
                     int enchantGap = XP.getSkillReqGap( player, XP.getEnchantsUseReq( player.getHeldItemMainhand() ) );
                     int gap = Math.max( weaponGap, enchantGap );
