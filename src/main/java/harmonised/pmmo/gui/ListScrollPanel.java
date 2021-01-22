42853c86d61d98dd9846928dfc9fe24c03876284
Harmonised <harmonised7@gmail.com>
2021-01-22 12:02:57 +0000

    Hiscore Place Display

--- src/main/java/harmonised/pmmo/gui/ListScrollPanel.java
+++ src/main/java/harmonised/pmmo/gui/ListScrollPanel.java
@@ -65,8 +65,9 @@ public class ListScrollPanel extends GuiScreen
     protected void drawPanel(int entryRight, int relativeY, Tessellator tess, int mouseX, int mouseY)
     {
         accumulativeHeight = 0;
-        for( ListButton button : buttons )
+        for( int i = 0; i < buttons.size(); i++ )
         {
+            ListButton button = buttons.get( i );
             button.x = this.right - button.width - 8;
             button.y = relativeY + accumulativeHeight;
 
@@ -88,12 +89,15 @@ public class ListScrollPanel extends GuiScreen
                         color = 0x54fc54;
                 }
 
+                if( jType.equals( JType.SKILLS ) || jType.equals( JType.HISCORE ) )
+                    drawString( Minecraft.getMinecraft().fontRenderer, (i+1) + ".", this.left + 178, button.y + 2, color );
+
                 drawString( Minecraft.getMinecraft().fontRenderer, button.title, this.left + 6, button.y + 2, color );
 
-                int i = 0;
+                int j = 0;
                 for( String line : button.text )
                 {
-                    drawString( mc.fontRenderer, line, this.left + 6, button.y + 11 + (i++ * 9), 0xffffff );
+                    drawString( mc.fontRenderer, line, this.left + 6, button.y + 11 + (j++ * 9), 0xffffff );
                 }
             }
             accumulativeHeight += button.getHeight() + 4;
