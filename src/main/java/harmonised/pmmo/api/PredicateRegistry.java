2d634db11e14d6e61edf6ccaab84f526f7c10d34
Caltinor <62700786+Caltinor@users.noreply.github.com>
2021-04-14 19:24:16 -0400

    javadocs and null checker

--- src/main/java/harmonised/pmmo/api/PredicateRegistry.java
+++ src/main/java/harmonised/pmmo/api/PredicateRegistry.java
@@ -14,6 +14,16 @@ public class PredicateRegistry {
 	
 	private static Map<String, Predicate<PlayerEntity>> reqPredicates = new HashMap<>();
 	
+	/** registers a predicate to be used in determining if a given player is permitted
+	 * to perform a particular action.  The ResouceLocation and JType parameters are 
+	 * conditions for when this check should be applied and are used by PMMO to know
+	 * which predicates apply in which contexts.  The predicate itself links to the 
+	 * compat mod's logic which handles the external behavior.
+	 * 
+	 * @param res the block, item, or entity registrykey
+	 * @param jType the PMMO behavior type
+	 * @param pred what executes to determine if player is permitted to perform the action
+	 */
 	public static void registerPredicate(ResourceLocation res, JType jType, Predicate<PlayerEntity> pred) 
 	{
 		String condition = jType.toString()+";"+res.toString();
@@ -23,11 +33,25 @@ public class PredicateRegistry {
 		XP.LOGGER.info("Predicate Registered: "+condition);
 	}
 	
+	/**this is an internal method to check if a predicate exists for the given conditions
+	 * 
+	 * @param res res the block, item, or entity registrykey
+	 * @param jType the PMMO behavior type
+	 * @return whether or not a predicate is registered for the parameters
+	 */
 	public static boolean predicateExists(ResourceLocation res, JType jType) 
 	{
 		return reqPredicates.containsKey(jType.toString()+";"+res.toString());
 	}
 	
+	/**this is executed by PMMO logic to determine if the player is permitted to perform
+	 * the action according to the object and type contexts.  
+	 * 
+	 * @param player the player performing the action
+	 * @param res res res the block, item, or entity registrykey
+	 * @param jType the PMMO behavior type
+	 * @return whether the player is permitted to do the action (true if yes)
+	 */
 	public static boolean checkPredicateReq(PlayerEntity player, ResourceLocation res, JType jType) 
 	{
 		return reqPredicates.get(jType.toString()+";"+res.toString()).test(player); 
--- src/main/java/harmonised/pmmo/api/TooltipSupplier.java
+++ src/main/java/harmonised/pmmo/api/TooltipSupplier.java
@@ -16,6 +16,17 @@ public class TooltipSupplier {
 	public static final Logger LOGGER = LogManager.getLogger();
 	private static Map<JType, Map<ResourceLocation, Function<ItemStack, Map<String, Double>>>> tooltips = new HashMap<>();
 	
+	/**registers a Function to be used in providing the requirements for specific item
+	 * skill requirements. The map consists of skill name and skill value pairs.  
+	 * The ResouceLocation and JType parameters are conditions for when this check
+	 * should be applied and are used by PMMO to know which functions apply in which
+	 * contexts.  The function itself links to the compat mod's logic which handles 
+	 * the external behavior.
+	 *  
+	 * @param res the block, item, or entity registrykey
+	 * @param jType the PMMO behavior type
+	 * @param func returns a map of skills and required levels
+	 */
 	public static void registerTooltipData(ResourceLocation res, JType jType, Function<ItemStack, Map<String, Double>> func) 
 	{
 		if (func == null) {LOGGER.info("Supplied Function Null"); return;}
@@ -31,6 +42,12 @@ public class TooltipSupplier {
 		tooltips.get(jType).put(res, func);
 	}
 	
+	/**this is an internal method to check if a function exists for the given conditions
+	 * 
+	 * @param res res the block, item, or entity registrykey
+	 * @param jType the PMMO behavior type
+	 * @return whether or not a function is registered for the parameters
+	 */
 	public static boolean tooltipExists(ResourceLocation res, JType jType)
 	{
 		if (jType == null) return false;
@@ -39,10 +56,20 @@ public class TooltipSupplier {
 		return tooltips.get(jType).containsKey(res);
 	}
 	
+	/**this is executed by PMMO where the requirde map for tooltips is used.  some PMMO
+	 * behavior uses this map before a a requirement check for adding things like dynamic
+	 * values. 
+	 * 
+	 * @param res res the block, item, or entity registrykey
+	 * @param jType the PMMO behavior type
+	 * @param stack the itemstack being evaluated for skill requirements
+	 * @return the skill requirements of the item.
+	 */	
 	public static Map<String, Double> getTooltipData(ResourceLocation res, JType jType, ItemStack stack)
 	{
 		if (tooltipExists(res, jType)) {
-			return tooltips.get(jType).get(res).apply(stack);
+			Map<String, Double> suppliedData = tooltips.get(jType).get(res).apply(stack);
+			return suppliedData == null ? new HashMap<>() : suppliedData;
 		}				
 		return JsonConfig.data.get(jType).get(res.toString());
 	}
