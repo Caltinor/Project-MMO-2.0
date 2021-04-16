3bffd7c97cc6312ae0801b0b2089873cb0949f41
Caltinor <62700786+Caltinor@users.noreply.github.com>
2021-04-16 10:25:36 -0400

    removed unneeded line that created load order locking

--- src/main/java/harmonised/pmmo/api/PredicateRegistry.java
+++ src/main/java/harmonised/pmmo/api/PredicateRegistry.java
@@ -5,7 +5,6 @@ import java.util.Map;
 import java.util.function.Predicate;
 
 import harmonised.pmmo.config.JType;
-import harmonised.pmmo.config.JsonConfig;
 import harmonised.pmmo.util.XP;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.ResourceLocation;
@@ -27,9 +26,9 @@ public class PredicateRegistry {
 	public static void registerPredicate(ResourceLocation res, JType jType, Predicate<PlayerEntity> pred) 
 	{
 		String condition = jType.toString()+";"+res.toString();
-		if (pred == null) return;
-		reqPredicates.put(condition, pred);
-		JsonConfig.data.get(jType).remove(res.toString());
+		if ( pred == null ) 
+			return;
+		reqPredicates.put( condition, pred );
 		XP.LOGGER.info("Predicate Registered: "+condition);
 	}
 	
@@ -41,7 +40,7 @@ public class PredicateRegistry {
 	 */
 	public static boolean predicateExists(ResourceLocation res, JType jType) 
 	{
-		return reqPredicates.containsKey(jType.toString()+";"+res.toString());
+		return reqPredicates.containsKey( jType.toString()+";"+res.toString() );
 	}
 	
 	/**this is executed by PMMO logic to determine if the player is permitted to perform
@@ -54,6 +53,8 @@ public class PredicateRegistry {
 	 */
 	public static boolean checkPredicateReq(PlayerEntity player, ResourceLocation res, JType jType) 
 	{
-		return reqPredicates.get(jType.toString()+";"+res.toString()).test(player); 
+		if ( !predicateExists( res, jType ) ) 
+			return false;
+		return reqPredicates.get( jType.toString()+";"+res.toString() ).test(player); 
 	}
 }
