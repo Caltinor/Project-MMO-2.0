ce7597b88d9c34f65f65be362c646466abcefa1f
Caltinor <62700786+Caltinor@users.noreply.github.com>
2022-02-08 17:24:27 -0500

    Crafting Fix
    Fixes crafting reqs not applying correctly.  Both sides now evaluate, and the req and cancellation have been reintroduced.

--- src/main/java/harmonised/pmmo/events/CraftedHandler.java
+++ src/main/java/harmonised/pmmo/events/CraftedHandler.java
@@ -1,11 +1,13 @@
 package harmonised.pmmo.events;
 
+import harmonised.pmmo.api.APIUtils;
+import harmonised.pmmo.api.PredicateRegistry;
 import harmonised.pmmo.config.Config;
 import harmonised.pmmo.config.JType;
 import harmonised.pmmo.gui.WorldXpDrop;
-import harmonised.pmmo.skills.Skill;
 import harmonised.pmmo.util.XP;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.phys.Vec3;
@@ -23,39 +25,44 @@ public class CraftedHandler
     {
         try
         {
-            if(event.getPlayer() instanceof ServerPlayer)
+            Player player = event.getPlayer();
+            ItemStack itemStack = event.getCrafting();
+            
+            if (!PredicateRegistry.checkPredicateReq(player, itemStack.getItem().getRegistryName(), JType.REQ_CRAFT)) {
+            	event.setCanceled(true);
+            	return;
+            }
+            
+            Vec3 pos = player.position();
+            double defaultCraftingXp = Config.forgeConfig.defaultCraftingXp.get();
+            double durabilityMultiplier = 1;
+
+            Map<String, Double> xpValue = XP.getXp(itemStack, JType.XP_VALUE_CRAFT);
+
+            Map<String, Double> award = new HashMap<>();
+            if(xpValue.size() == 0)
             {
-                ServerPlayer player = (ServerPlayer) event.getPlayer();
-                Vec3 pos = player.position();
-                double defaultCraftingXp = Config.forgeConfig.defaultCraftingXp.get();
-                double durabilityMultiplier = 1;
-
-                ItemStack itemStack = event.getCrafting();
-                Map<String, Double> xpValue = XP.getXp(itemStack, JType.XP_VALUE_CRAFT);
-
-                Map<String, Double> award = new HashMap<>();
-                if(xpValue.size() == 0)
-                {
-                    if(itemStack.getItem() instanceof BlockItem)
-                        award.put("crafting", (double) ((BlockItem) itemStack.getItem()).getBlock().defaultBlockState().getDestroySpeed(null, null));
-                    else
-                        award.put("crafting", defaultCraftingXp);
-                }
+                if(itemStack.getItem() instanceof BlockItem)
+                    award.put("crafting", (double) ((BlockItem) itemStack.getItem()).getBlock().defaultBlockState().getDestroySpeed(null, null));
                 else
-                    XP.addMapsAnyDouble(award, xpValue);
+                    award.put("crafting", defaultCraftingXp);
+            }
+            else
+                XP.addMapsAnyDouble(award, xpValue);
 
-                if(itemStack.isDamageableItem())
-                    durabilityMultiplier = (double) (itemStack.getMaxDamage() - itemStack.getDamageValue()) / (double) itemStack.getMaxDamage();
+            if(itemStack.isDamageableItem())
+                durabilityMultiplier = (double) (itemStack.getMaxDamage() - itemStack.getDamageValue()) / (double) itemStack.getMaxDamage();
 
 //            XP.multiplyMap(award, itemStack.getCount());
-                XP.multiplyMapAnyDouble(award, durabilityMultiplier);
+            XP.multiplyMapAnyDouble(award, durabilityMultiplier);
 
-                for(String awardSkillName : award.keySet())
-                {
-                    WorldXpDrop xpDrop = WorldXpDrop.fromXYZ(XP.getDimResLoc(player.getLevel()), pos.x(), pos.y() + player.getEyeHeight() + 0.523, pos.z(), 1.523, award.get(awardSkillName), awardSkillName);
-                    XP.addWorldXpDrop(xpDrop, player);
-                    Skill.addXp(awardSkillName, player, award.get(awardSkillName), "crafting", false, false);
-                }
+            for(String awardSkillName : award.keySet())
+            {
+            	if (player instanceof ServerPlayer) {
+            		WorldXpDrop xpDrop = WorldXpDrop.fromXYZ(XP.getDimResLoc(player.getLevel()), pos.x(), pos.y() + player.getEyeHeight() + 0.523, pos.z(), 1.523, award.get(awardSkillName), awardSkillName);
+                	XP.addWorldXpDrop(xpDrop, (ServerPlayer) player);
+            	}
+                APIUtils.addXp(awardSkillName, player.getUUID(), award.get(awardSkillName), "crafting", false, false);
             }
         }
         catch(Exception e)
