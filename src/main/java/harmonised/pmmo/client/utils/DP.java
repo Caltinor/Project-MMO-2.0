fa4778b966a617b7776fc4db3145a0880842041a
Caltinor <62700786+Caltinor@users.noreply.github.com>
2022-02-22 17:39:11 -0500

    fix raw XP display

--- src/main/java/harmonised/pmmo/client/utils/DP.java
+++ src/main/java/harmonised/pmmo/client/utils/DP.java
@@ -4,83 +4,19 @@ import java.util.Locale;
 
 public class DP
 {	
+	private static char[] prefix = {' ','k','m','g','t','p','e','z','y'};
 	public static String dprefix(long input)
 	{
-		char prefix = ' ';
-
-		if(input >= 1E+3)
-		{
-			if(input >= 1E+6)
-			{
-				if(input >= 1E+9)
-				{
-					if(input >= 1E+12)
-					{
-						if(input >= 1E+15)
-						{
-							if(input >= 1E+18)
-							{
-								if(input >= 1E+21)
-								{
-									if(input >= 1E+24)
-									{
-										if(input >= 1E+27)
-										{
-											return String.valueOf(input);
-										}
-										else
-										{
-											input /= 1E+24;
-											prefix = 'y';
-										}
-									}
-									else
-									{
-										input /= 1E+21;
-										prefix = 'z';
-									}
-								}
-								else
-								{
-									input /= 1E+18;
-									prefix = 'e';
-								}
-							}
-							else
-							{
-								input /= 1E+15;
-								prefix = 'p';
-							}
-						}
-						else
-						{
-							input /= 1E+12;
-							prefix = 't';
-						}
-					}
-					else
-					{
-						input /= 1E+9;
-						prefix = 'g';
-					}
-				}
-				else
-				{
-					input /= 1E+6;
-					prefix = 'm';
-				}
-			}
-			else
-			{
-				input /= 1E+3;
-				prefix = 'k';
-			}
-		}
+		int length = String.valueOf(input).length();
+		int prefixId = length / 3;
+		prefixId = length % 3 == 0 ? prefixId - 1 : prefixId;
+		input /= Math.pow(10, (length - 3));
+		Double output = Double.valueOf(input);	
+		int decimalPlaces = 3 - (length - (length/3) * 3);
+		decimalPlaces = decimalPlaces == 3 ? 0 : decimalPlaces;
+		output /= Math.pow(10, decimalPlaces);
 		
-		if(prefix == ' ')
-			return String.format(Locale.ENGLISH, "%d", input);
-		else
-			return String.format(Locale.ENGLISH, "%d", input) + prefix;
+		return String.format(Locale.ENGLISH, "%."+decimalPlaces+"f", output) + prefix[prefixId];
 	}
 	
 	public static String dp(Float input)
@@ -95,7 +31,7 @@ public class DP
 
 	public static String dpCustom(Double input, int decPlaces)
 	{
-		return String.format(Locale.ENGLISH, "%" + decPlaces + "f", input);
+		return String.format(Locale.ENGLISH, "%." + decPlaces + "f", input);
 	}
 
 	public static String dpSoft(double input)
