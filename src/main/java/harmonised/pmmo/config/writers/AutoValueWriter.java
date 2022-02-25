06dc9d2d9b5ed89535c56294af687b548ba7cf01
Caltinor <62700786+Caltinor@users.noreply.github.com>
2022-02-25 13:01:01 -0500

    staged future config dumper feature

--- src/main/java/harmonised/pmmo/config/writers/AutoValueWriter.java
+++ src/main/java/harmonised/pmmo/config/writers/AutoValueWriter.java
@@ -1,10 +1,14 @@
 package harmonised.pmmo.config.writers;
 
-public class AutoValueWriter {
-	public static boolean writeAutoValues = false;
+/* DESIGN:  
+ * This was originally intended to be a class for adding runtime values to the configuration files based on a command-toggled
+ * setting to do so.  It would accumulate settings and then write them to file when the server shut down.  Since the move to
+ * datapacks, this is rather impractical, however players will likely still appreciate having a system for exporting the runtime
+ * settings to a file which they can then use in a datapack since we cannot commit the changes directly to the configuration. 
+ */
+public class AutoValueWriter {	
 	
-	public static void writeGeneratedAutoValuesToConfigFiles() {
+	public static void dumpObjectConfigToFile() {
 		
 	}
-
 }
