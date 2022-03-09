76ce02b612af43a33a9a8fff1d80cef7b1f1aac4
Caltinor <62700786+Caltinor@users.noreply.github.com>
2022-03-09 17:12:40 -0500

    Anvil Repair Event

---
+++ src/main/java/harmonised/pmmo/events/impl/AnvilRepairHandler.java
@@ -0,0 +1,30 @@
+package harmonised.pmmo.events.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import harmonised.pmmo.api.enums.EventType;
+import harmonised.pmmo.core.Core;
+import harmonised.pmmo.features.party.PartyUtils;
+import harmonised.pmmo.util.TagUtils;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraftforge.event.entity.player.AnvilRepairEvent;
+
+public class AnvilRepairHandler {
+
+	public static void handle(AnvilRepairEvent event) {
+		Core core = Core.get(event.getPlayer().getLevel());
+		CompoundTag eventHookOutput = new CompoundTag();
+		boolean serverSide = !event.getPlayer().level.isClientSide; 
+		if (serverSide)		
+			eventHookOutput = core.getEventTriggerRegistry().executeEventListeners(EventType.ANVIL_REPAIR, event, new CompoundTag());
+		//Process perks
+		CompoundTag perkOutput = TagUtils.mergeTags(eventHookOutput, core.getPerkRegistry().executePerk(EventType.CRAFT, event.getPlayer(), eventHookOutput, core.getSide()));
+		if (serverSide) {
+			Map<String, Long> xpAward = core.getExperienceAwards(EventType.ANVIL_REPAIR, event.getItemResult(), event.getPlayer(), perkOutput);
+			List<ServerPlayer> partyMembersInRange = PartyUtils.getPartyMembersInRange((ServerPlayer) event.getPlayer());
+			core.awardXP(partyMembersInRange, xpAward);	
+		}
+	}
+}
