0e3eb55580bcde7b64c26294308ad443441f5b6c
Caltinor <62700786+Caltinor@users.noreply.github.com>
2022-05-04 07:16:25 -0400

    MsLoggy fix

--- src/main/java/harmonised/pmmo/config/Config.java
+++ src/main/java/harmonised/pmmo/config/Config.java
@@ -157,17 +157,17 @@ public class Config {
 		
 		INFO_LOGGING = builder.comment("Which MsLoggy info logging should be enabled?  This will flood your log with data, but provides essential details",
 									  " when trying to find data errors and bug fixing.  ")
-						.define("Info Logging", List.of(LOG_CODE.LOADING.code,LOG_CODE.NETWORK.code,LOG_CODE.API.code), s -> s instanceof String);
+						.define("Info Logging", new ArrayList<>(List.of(LOG_CODE.LOADING.code,LOG_CODE.NETWORK.code,LOG_CODE.API.code)), s -> s instanceof String);
 		DEBUG_LOGGING = builder.comment("Which MsLoggy debug logging should be enabled?  This will flood your log with data, but provides essential details",
 				  					  " when trying to find bugs. DEVELOPER SETTING (mostly).  ")
-						.define("Debug Logging", List.of(), s -> s instanceof String);
+						.define("Debug Logging", new ArrayList<>(), s -> s instanceof String);
 		WARN_LOGGING = builder.comment("Which MsLoggy warn logging should be enabled?  This log type is helpful for catching important but non-fatal issues")
-						.define("Warn Logging", List.of(LOG_CODE.API.code), s -> s instanceof String);
+						.define("Warn Logging", new ArrayList<>(List.of(LOG_CODE.API.code)), s -> s instanceof String);
 		ERROR_LOGGING = builder.comment("Which Error Logging should be enabled.  it is highly recommended this stay true.  however, you can",
 									  "disable it to remove pmmo errors from the log.")
-						.define("Error Logging", List.of(LOG_CODE.DATA.code, LOG_CODE.API.code), s -> s instanceof String);
+						.define("Error Logging", new ArrayList<>(List.of(LOG_CODE.DATA.code, LOG_CODE.API.code)), s -> s instanceof String);
 		FATAL_LOGGING = builder.comment("Which MsLoggy fatal logging should be enabled?  I can't imagine a situation where you'd want this off, but here you go.")
-						.define("Fatal Logging", List.of(LOG_CODE.API.code), s -> s instanceof String);
+						.define("Fatal Logging", new ArrayList<>(List.of(LOG_CODE.API.code)), s -> s instanceof String);
 		
 		builder.pop(); //Ms. Loggy Block
 	}
