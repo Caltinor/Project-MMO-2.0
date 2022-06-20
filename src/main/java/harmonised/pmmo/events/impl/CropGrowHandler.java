a0e301041f85e66572d5bd3023cbe7610fb3db63
Caltinor <62700786+Caltinor@users.noreply.github.com>
2022-06-20 17:14:34 -0400

    Fix Crop crash on Player cast

--- src/main/java/harmonised/pmmo/events/impl/CropGrowHandler.java
+++ src/main/java/harmonised/pmmo/events/impl/CropGrowHandler.java
@@ -19,16 +19,16 @@ public class CropGrowHandler {
 
 	public static void handle(CropGrowEvent.Post event) {
 		Level level = (Level) event.getWorld();
-		Core core = Core.get(level);
-		UUID placerID = level.getChunkAt(event.getPos()).getCapability(ChunkDataProvider.CHUNK_CAP).orElseGet(ChunkDataHandler::new).checkPos(event.getPos());
-		ServerPlayer player = event.getWorld().getServer().getPlayerList().getPlayer(placerID);
-		CompoundTag hookOutput = new CompoundTag();
-		if (!level.isClientSide)
-			hookOutput = core.getEventTriggerRegistry().executeEventListeners(EventType.GROW, event, new CompoundTag());
-		hookOutput = TagUtils.mergeTags(hookOutput, core.getPerkRegistry().executePerk(EventType.GROW, player, core.getSide()));
-		if (!level.isClientSide && placerID != null) {
+		if (!level.isClientSide) {
+			Core core = Core.get(level);
+			UUID placerID = level.getChunkAt(event.getPos()).getCapability(ChunkDataProvider.CHUNK_CAP).orElseGet(ChunkDataHandler::new).checkPos(event.getPos());
+			ServerPlayer player = event.getWorld().getServer().getPlayerList().getPlayer(placerID);
+			if (player == null) return;
+			CompoundTag hookOutput = core.getEventTriggerRegistry().executeEventListeners(EventType.GROW, event, new CompoundTag());
+			hookOutput = TagUtils.mergeTags(hookOutput, core.getPerkRegistry().executePerk(EventType.GROW, player, core.getSide()));
+			//Apply experience gains
 			Map<String, Long> xpAward = core.getBlockExperienceAwards(EventType.GROW, event.getPos(), level, player, hookOutput);
-			List<ServerPlayer> partyMembersInRange = PartyUtils.getPartyMembersInRange((ServerPlayer) player);
+			List<ServerPlayer> partyMembersInRange = PartyUtils.getPartyMembersInRange(player);
 			core.awardXP(partyMembersInRange, xpAward);
 		}
 	}
