4986942525ee1c209947a63e9fce15ec08a93949
Caltinor <62700786+Caltinor@users.noreply.github.com>
2022-07-06 16:48:33 -0400

    cleanup

--- src/main/java/harmonised/pmmo/setup/datagen/LangProvider.java
+++ src/main/java/harmonised/pmmo/setup/datagen/LangProvider.java
@@ -110,6 +110,9 @@ public class LangProvider extends LanguageProvider{
 		public TranslatableComponent asComponent() {
 			return new TranslatableComponent(key());
 		}
+		public MutableComponent asComponent(Object...obj) {
+			return Component.translatable(key(), obj);
+		}
 		public static class Builder {
 			String key;
 			Map<String, String> localeMap;
