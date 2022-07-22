c19473c884a1285db9d7b9d9ac0b73bb63b7155e
Caltinor <62700786+Caltinor@users.noreply.github.com>
2022-07-21 20:27:42 -0400

    Revert "change event bus  fixes #47"
    This reverts commit 525b548c00dc0354dda1cc28ffbba3ac21c34eb9.

--- src/main/java/harmonised/pmmo/setup/ClientSetup.java
+++ src/main/java/harmonised/pmmo/setup/ClientSetup.java
@@ -6,12 +6,13 @@ import harmonised.pmmo.client.gui.XPOverlayGUI;
 import harmonised.pmmo.util.Reference;
 import net.minecraft.client.KeyMapping;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
-import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.client.ClientRegistry;
+import net.minecraftforge.client.gui.IIngameOverlay;
+import net.minecraftforge.client.gui.OverlayRegistry;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
-@Mod.EventBusSubscriber(modid=Reference.MOD_ID, bus=Mod.EventBusSubscriber.Bus.MOD, value=Dist.CLIENT)
+@Mod.EventBusSubscriber(modid=Reference.MOD_ID, bus=Mod.EventBusSubscriber.Bus.FORGE, value=Dist.CLIENT)
 public class ClientSetup {
 
 	public static final KeyMapping SHOW_VEIN = new KeyMapping("key.pmmo.showVein", GLFW.GLFW_KEY_TAB, "category.pmmo");
@@ -23,16 +24,12 @@ public class ClientSetup {
     public static final KeyMapping OPEN_SKILLS = new KeyMapping("key.pmmo.openSkills", GLFW.GLFW_KEY_UNKNOWN, "category.pmmo");
     public static final KeyMapping OPEN_GLOSSARY = new KeyMapping("key.pmmo.openGlossary", GLFW.GLFW_KEY_UNKNOWN, "category.pmmo");
     
-    @SubscribeEvent
-    public static void init(RegisterKeyMappingsEvent event) {
-    	event.register(SHOW_VEIN);
-    	event.register(SHOW_LIST);
-    	event.register(VEIN_KEY);
-    	event.register(OPEN_MENU);
-    }
+    public static final IIngameOverlay STATS_OVERLAY = OverlayRegistry.registerOverlayTop("stats_overlay", new XPOverlayGUI());
     
-    @SubscribeEvent
-    public static void registerOverlay(RegisterGuiOverlaysEvent event) {
-    	event.registerAboveAll("stats_overlay", new XPOverlayGUI());
+    public static void init(FMLClientSetupEvent event) {
+    	ClientRegistry.registerKeyBinding(SHOW_VEIN);
+    	ClientRegistry.registerKeyBinding(SHOW_LIST);
+    	ClientRegistry.registerKeyBinding(VEIN_KEY);
+    	ClientRegistry.registerKeyBinding(OPEN_MENU);
     }
 }
