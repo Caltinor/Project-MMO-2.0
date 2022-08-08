b2953ba41f1e3bcf78c6ee875f3b89084de10699
Caltinor <62700786+Caltinor@users.noreply.github.com>
2022-08-08 17:02:53 -0400

    dig speed after elevation change fixes #59

--- src/main/java/harmonised/pmmo/events/impl/BreakSpeedHandler.java
+++ src/main/java/harmonised/pmmo/events/impl/BreakSpeedHandler.java
@@ -11,7 +11,7 @@ import harmonised.pmmo.core.Core;
 import harmonised.pmmo.util.Messenger;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.event.entity.player.PlayerEvent.BreakSpeed;
@@ -24,25 +24,25 @@ public class BreakSpeedHandler {
 	public static void handle(BreakSpeed event) {
 		Core core = Core.get(event.getEntity().getLevel());
 		//First, check the cache for a repeat event trigger
-		if (resultCache.containsKey(event.getPlayer().getUUID())) {
+		if (resultCache.containsKey(event.getEntity().getUUID())) {
 			if (usingCache(event)) return;
 		}
 		//calculate the event results anew.
-		if (!core.isActionPermitted(ReqType.TOOL, event.getPlayer().getMainHandItem(), event.getPlayer())) {
+		if (!core.isActionPermitted(ReqType.TOOL, event.getEntity().getMainHandItem(), event.getEntity())) {
 			event.setCanceled(true);
-			event.getPlayer().displayClientMessage(new TextComponent("Unable to use this tool"), false);
-			Messenger.sendDenialMsg(ReqType.TOOL, event.getPlayer(), event.getPlayer().getMainHandItem().getDisplayName());
+			event.getEntity().displayClientMessage(Component.literal("Unable to use this tool"), false);
+			Messenger.sendDenialMsg(ReqType.TOOL, event.getEntity(), event.getEntity().getMainHandItem().getDisplayName());
 			//Cache the result for future event occurrences
-			resultCache.put(event.getPlayer().getUUID(), 
-					new DetailsCache(event.getPlayer().getMainHandItem(), event.getPos(), event.getState(), true, event.getOriginalSpeed()));
+			resultCache.put(event.getEntity().getUUID(), 
+					new DetailsCache(event.getEntity().getMainHandItem(), event.getPos(), event.getState(), true, event.getOriginalSpeed()));
 			return;
 		}
-		if (!core.isBlockActionPermitted(ReqType.BREAK, event.getPos(), event.getPlayer())) {
+		if (!core.isBlockActionPermitted(ReqType.BREAK, event.getPos(), event.getEntity())) {
 			event.setCanceled(true);
-			event.getPlayer().displayClientMessage(new TextComponent("Unable to break this block"), false);
-			Messenger.sendDenialMsg(ReqType.BREAK, event.getPlayer(), event.getState().getBlock().getName());
-			resultCache.put(event.getPlayer().getUUID(), 
-					new DetailsCache(event.getPlayer().getMainHandItem(), event.getPos(), event.getState(), true, event.getOriginalSpeed()));
+			event.getEntity().displayClientMessage(Component.literal("Unable to break this block"), false);
+			Messenger.sendDenialMsg(ReqType.BREAK, event.getEntity(), event.getState().getBlock().getName());
+			resultCache.put(event.getEntity().getUUID(), 
+					new DetailsCache(event.getEntity().getMainHandItem(), event.getPos(), event.getState(), true, event.getOriginalSpeed()));
 			return;
 		}
 		CompoundTag eventHookOutput = new CompoundTag();
@@ -50,21 +50,21 @@ public class BreakSpeedHandler {
 			eventHookOutput = getEventHookResults(core, event);
 			if (eventHookOutput.getBoolean(APIUtils.IS_CANCELLED)) {
 				event.setCanceled(true);
-				resultCache.put(event.getPlayer().getUUID(), 
-						new DetailsCache(event.getPlayer().getMainHandItem(), event.getPos(), event.getState(), true, event.getOriginalSpeed()));
+				resultCache.put(event.getEntity().getUUID(), 
+						new DetailsCache(event.getEntity().getMainHandItem(), event.getPos(), event.getState(), true, event.getOriginalSpeed()));
 				return;
 			}
 		}
 		CompoundTag perkDataIn = eventHookOutput;
 		perkDataIn.putFloat(APIUtils.BREAK_SPEED_INPUT_VALUE, event.getOriginalSpeed());
 		perkDataIn.putLong(APIUtils.BLOCK_POS, event.getPos().asLong());
-		//how am i gonna do gaps?  hmmmm
-		CompoundTag perkDataOut = core.getPerkRegistry().executePerk(EventType.BREAK_SPEED, event.getPlayer(), perkDataIn, core.getSide());
+
+		CompoundTag perkDataOut = core.getPerkRegistry().executePerk(EventType.BREAK_SPEED, event.getEntity(), perkDataIn, core.getSide());
 		if (perkDataOut.contains(APIUtils.BREAK_SPEED_OUTPUT_VALUE)) {
 			float newSpeed = Math.max(0, perkDataOut.getFloat(APIUtils.BREAK_SPEED_OUTPUT_VALUE));
 			event.setNewSpeed(newSpeed);
-			resultCache.put(event.getPlayer().getUUID(), 
-					new DetailsCache(event.getPlayer().getMainHandItem(), event.getPos(), event.getState(), false, event.getNewSpeed()));
+			resultCache.put(event.getEntity().getUUID(), 
+					new DetailsCache(event.getEntity().getMainHandItem(), event.getPos(), event.getState(), false, newSpeed));
 		}			
 	}
 	
@@ -73,10 +73,10 @@ public class BreakSpeedHandler {
 	}
 
 	private static boolean usingCache(BreakSpeed event) {
-		DetailsCache cachedData = resultCache.get(event.getPlayer().getUUID());
+		DetailsCache cachedData = resultCache.get(event.getEntity().getUUID());
 		if (event.getPos().equals(cachedData.pos)
 			&& event.getState().equals(cachedData.state)
-			&& event.getPlayer().getMainHandItem().equals(cachedData.item, false)) {			
+			&& event.getEntity().getMainHandItem().equals(cachedData.item, false)) {			
 			if (cachedData.cancelled) 
 				event.setCanceled(true);
 			else 
