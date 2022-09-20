2bc9c931d26c493deb5ced536fd9a8eb8e01fed7
Caltinor <62700786+Caltinor@users.noreply.github.com>
2022-09-20 17:09:46 -0400

    Readd Curio Compat

--- src/main/java/harmonised/pmmo/compat/curios/CurioCompat.java
+++ src/main/java/harmonised/pmmo/compat/curios/CurioCompat.java
@@ -1,23 +1,22 @@
 package harmonised.pmmo.compat.curios;
 
-//import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.List;
 
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-//import top.theillusivec4.curios.api.CuriosApi;
+import top.theillusivec4.curios.api.CuriosApi;
 
 public class CurioCompat {
 	public static boolean hasCurio = false;
 	//provide curio data to various methods
 	public static List<ItemStack> getItems(Player player) {
-		/*List<ItemStack> curioItems = new ArrayList<>();
+		List<ItemStack> curioItems = new ArrayList<>();
 		CuriosApi.getCuriosHelper().getEquippedCurios(player).ifPresent((handler) -> {
 			for (int i = 0; i < handler.getSlots(); i++) {
 				curioItems.add(handler.getStackInSlot(i));
 			}
 		});
-		return curioItems;*/
-		return List.of();
+		return curioItems;
 	}
 }
