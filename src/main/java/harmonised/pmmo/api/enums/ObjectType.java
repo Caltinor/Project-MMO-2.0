02b55995667dd7b9b6d9601bddc8798de8edc16b
Caltinor <62700786+Caltinor@users.noreply.github.com>
2022-10-29 07:35:57 -0400

    CT base setup complete

--- src/main/java/harmonised/pmmo/api/enums/ObjectType.java
+++ src/main/java/harmonised/pmmo/api/enums/ObjectType.java
@@ -12,7 +12,11 @@ import net.minecraftforge.common.IExtensibleEnum;
 public enum ObjectType implements StringRepresentable, IExtensibleEnum{
 	ITEM,
 	BLOCK,
-	ENTITY;
+	ENTITY,
+	DIMENSION,
+	BIOME,
+	ENCHANTMENT,
+	PLAYER;
 	
 	public static final Codec<ObjectType> CODEC = IExtensibleEnum.createCodecForExtensibleEnum(ObjectType::values, ObjectType::byName);
 	private static final Map<String, ObjectType> BY_NAME = Arrays.stream(values()).collect(Collectors.toMap(ObjectType::getSerializedName, s -> s));
