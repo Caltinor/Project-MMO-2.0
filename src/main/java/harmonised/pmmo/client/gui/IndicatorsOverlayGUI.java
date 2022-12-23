618620d775ba7bff30cc1882a4986ea5b04eb1d1
Caltinor <62700786+Caltinor@users.noreply.github.com>
2022-12-23 13:40:22 -0500

    Add vein indicators to crosshair.  Fixes #148

---
+++ src/main/java/harmonised/pmmo/client/gui/IndicatorsOverlayGUI.java
@@ -0,0 +1,36 @@
+package harmonised.pmmo.client.gui;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.PoseStack;
+
+import harmonised.pmmo.client.utils.VeinTracker;
+import harmonised.pmmo.util.Reference;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiComponent;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.client.gui.overlay.ForgeGui;
+import net.minecraftforge.client.gui.overlay.IGuiOverlay;
+
+public class IndicatorsOverlayGUI implements IGuiOverlay{
+	private static final ResourceLocation ICONS = new ResourceLocation(Reference.MOD_ID, "textures/gui/overlay_icons.png");
+	private Minecraft mc;
+	private BlockHitResult bhr;
+	
+	@Override
+	public void render(ForgeGui gui, PoseStack poseStack, float partialTick, int screenWidth, int screenHeight) {
+		if (mc == null)
+			mc = Minecraft.getInstance();
+		if (!(mc.hitResult instanceof BlockHitResult))
+			return;
+		else
+			bhr = (BlockHitResult) mc.hitResult;
+		
+		if(!mc.options.renderDebug && VeinTracker.isLookingAtVeinTarget(bhr)){
+			RenderSystem.setShaderTexture(0, ICONS);
+			float iconIndex = VeinTracker.mode.ordinal() * 16; //TODO Use VeinShape to set value
+			GuiComponent.blit(poseStack, (screenWidth/2)-16, (screenHeight/2)-8, iconIndex, 0f, 16, 16, 48, 16);
+		}
+	}
+
+}
