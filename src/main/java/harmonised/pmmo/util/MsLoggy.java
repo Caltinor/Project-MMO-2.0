a1aa12cbd40a5c3fbcd7c8b191e364c3beecc719
Caltinor <62700786+Caltinor@users.noreply.github.com>
2022-12-31 16:55:24 -0500

    improve MsLoggy

--- src/main/java/harmonised/pmmo/util/MsLoggy.java
+++ src/main/java/harmonised/pmmo/util/MsLoggy.java
@@ -1,38 +1,41 @@
 package harmonised.pmmo.util;
 
-import java.util.List;
+import java.util.Collection;
 import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.Predicate;
 import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.util.TriConsumer;
-
 import harmonised.pmmo.config.Config;
 
 public enum MsLoggy {
-	INFO((code, message, args) -> {
-		if (Config.INFO_LOGGING.get().contains(code.code))
-			LogManager.getLogger().info(message, args);
-	}), 
-	WARN((code, message, args) -> {
-		if (Config.WARN_LOGGING.get().contains(code.code))
-			LogManager.getLogger().warn(message, args);
-	}),  
-	DEBUG((code, message, args) -> {
-		if (Config.DEBUG_LOGGING.get().contains(code.code))
-			LogManager.getLogger().debug(message, args);
-	}), 
-	ERROR((code, message, args) -> {
-		if (Config.ERROR_LOGGING.get().contains(code.code))
-			LogManager.getLogger().error(message, args);
-	}), 
-	FATAL((code, message, args) -> {
-		if (Config.FATAL_LOGGING.get().contains(code.code))
-			LogManager.getLogger().fatal(message, args);
-	});
+	INFO(code -> Config.INFO_LOGGING.get().contains(code.code),
+		(message, args) -> LogManager.getLogger().info(message, args)), 
+	WARN(code -> Config.WARN_LOGGING.get().contains(code.code),
+		(message, args) -> LogManager.getLogger().warn(message, args)),  
+	DEBUG(code -> Config.DEBUG_LOGGING.get().contains(code.code),
+		(message, args) -> LogManager.getLogger().debug(message, args)), 
+	ERROR(code -> Config.ERROR_LOGGING.get().contains(code.code),
+		(message, args) -> LogManager.getLogger().error(message, args)), 
+	FATAL(code -> Config.FATAL_LOGGING.get().contains(code.code),
+		(message, args) -> LogManager.getLogger().fatal(message, args));
 		
-	private TriConsumer<LOG_CODE, String, Object[]> logExecutor;
-	public void log(LOG_CODE code, String message, Object... obj) {logExecutor.accept(code, message, obj);}
-	MsLoggy(TriConsumer<LOG_CODE, String, Object[]> logger) {this.logExecutor = logger;}	
+	private Predicate<LOG_CODE> validator;
+	private BiConsumer<String, Object[]> logExecutor;
+	MsLoggy(Predicate<LOG_CODE> validator, BiConsumer<String, Object[]> logger) {
+		this.validator = validator; 
+		this.logExecutor = logger;
+	}	
 	
+	/**<p>These codes are used exclusively by {@link MsLoggy} to
+	 * specify which game function category to log.  This combines
+	 * with the logging level to create a granular array of logging
+	 * options for users.</p>
+	 * <p>Internally this enum maps a code-referable value to a
+	 * config string.</p>
+	 * 
+	 * @author Caltinor
+	 *
+	 */
 	public static enum LOG_CODE {
 		API("api"),
 		AUTO_VALUES("autovalues"),
@@ -47,19 +50,88 @@ public enum MsLoggy {
 		//unused default
 		NONE("none");
 		
+		/**The config equivalent string for this logging code.*/
 		public String code;
 		LOG_CODE(String code) {this.code = code;}
+	}
+	
+	/**<p>Logs the provided string.</p>
+	 * <p>The string can be a formatted string, in which case
+	 * the varargs will be used as format arguments.</p>
+	 * 
+	 * @param code the code category this should log for
+	 * @param message the message to display
+	 * @param obj format arguments
+	 */
+	public void log(LOG_CODE code, String message, Object... obj) {
+		if (validator.test(code))
+			logExecutor.accept(message, obj);
+	}
+	
+	/**<p>For every entry in the collection, logs the provided string.</p>
+	 * <p>The string can be a formatted string, in which case
+	 * the varargs will be used as format arguments.</p>
+	 * <p><b>IMPORTANT: The entry value will always be the first vararg</b></p>
+	 * 
+	 * @param <T> the entry type
+	 * @param code the code category this should log for
+	 * @param array the collection being logged
+	 * @param message the message to display per entry
+	 * @param obj any static arguments supplied to each message
+	 */
+	public <T> void log(LOG_CODE code, Collection<T> array, String message, Object... obj) {
+		if (validator.test(code)) 
+			array.forEach(entry -> {
+				Object[] params = new Object[obj.length+1];
+				params[0] = entry;
+				for (int i = 0; i < obj.length; i++) {params[i+1] = obj[i];}
+				logExecutor.accept(message, params);
+			});
+		
+	}
+	
+	/**<p>For every entry in the map, logs the provided string.</p>
+	 * <p>The string can be a formatted string, in which case
+	 * the varargs will be used as format arguments.</p>
+	 * <p><b>IMPORTANT: The entry key and value will always be the 
+	 * first two varargs</b></p>
+	 * 
+	 * @param <K> the key type
+	 * @param <V> the value type
+	 * @param code the code category this should log for
+	 * @param map the map being logged
+	 * @param message the message to display for each entry
+	 * @param obj any static arguments supplied to each message
+	 */
+	public <K, V> void log(LOG_CODE code, Map<K, V> map, String message, Object... obj) {
+		if (validator.test(code))
+			map.forEach((key, value) -> {
+				Object[] params = new Object[obj.length+2];
+				params[0] = key;
+				params[1] = value;
+				for (int i = 0; i < obj.length; i++) {params[i+2] = obj[i];}
+				logExecutor.accept(message, params);
+			});
 	}
 	
 	//=============RETURNABLE LOGGERS=========================
 	//These loggers let you inline your logging
 	//========================================================
-	public <VALUE> VALUE logAndReturn(VALUE value, LOG_CODE code, String message) {
-		this.logExecutor.accept(code, message, List.of(value).toArray());
+	public <VALUE> VALUE logAndReturn(VALUE value, LOG_CODE code, String message, Object... obj) {
+		if (validator.test(code)) {
+			Object[] params = new Object[obj.length+1];
+			params[0] = value;
+			for (int i = 0; i < obj.length; i++) {params[i+1] = obj[i];}
+			this.logExecutor.accept(message, params);
+		}
 		return value;
 	}
 	//=============PRINTING UTILITIES=========================
-	
+	/**Creates a readable string from a map instance
+	 * 
+	 * @param map the map being parsed
+	 * @return the string of key-value pairs
+	 */
 	public static String mapToString(Map<?, ?> map) {
 		String out = "";
 		for (Map.Entry<?, ?> entry : map.entrySet()) {
@@ -68,10 +140,15 @@ public enum MsLoggy {
 		return out;
 	}
 	
-	public static String listToString(List<?> list) {
+	/**Creates a readable string from a collection instance
+	 * 
+	 * @param list the collection being parsed
+	 * @return the string of entries
+	 */
+	public static <T> String listToString(Collection<T> list) {
 		String out = "[";
-		for (int i = 0; i < list.size(); i++) {
-			out += list.get(i).toString();
+		for (T entry : list) {
+			out += entry.toString() +", ";
 		}
 		out += "]";
 		return out;
