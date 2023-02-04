7312702daf5424a0833e831575eaad3a62f8d87e
Caltinor <62700786+Caltinor@users.noreply.github.com>
2023-02-04 06:47:09 -0500

    autoEntity mobcategory sensitivity

--- src/main/java/harmonised/pmmo/features/autovalues/AutoEntity.java
+++ src/main/java/harmonised/pmmo/features/autovalues/AutoEntity.java
@@ -10,6 +10,7 @@ import harmonised.pmmo.util.Reference;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
@@ -24,36 +25,7 @@ public class AutoEntity {
 			EventType.SHIELD_BLOCK,	EventType.TAMING};
 
 	public static Map<String, Integer> processReqs(ReqType type, ResourceLocation entityID) {
-		//exit early if not an applicable type
-		//if (!type.entityApplicable)
-			return new HashMap<>();
-			
-		/*Map<String, Integer> outMap = new HashMap<>();
-		switch (type) {
-		case RIDE: {
-			if (ForgeRegistries.ENTITIES.getValue(entityID).is(Reference.RIDEABLE_TAG)) {
-				outMap.putAll(getReqMap(entityID, type));
-			}
-			break;
-		}
-		case TAME: {
-			if (ForgeRegistries.ENTITIES.getValue(entityID).is(Reference.TAMABLE_TAG)) {
-				outMap.putAll(getReqMap(entityID, type));
-			}
-			break;
-		}	
-		case BREED: {
-			if (ForgeRegistries.ENTITIES.getValue(entityID).is(Reference.TAMABLE_TAG)) {
-				outMap.putAll(getReqMap(entityID, type));
-			}
-			break;
-		}	
-		case ENTITY_INTERACT:{
-			outMap.putAll(getReqMap(entityID, type));
-			break;
-		}
-		default: }
-		return outMap;*/
+		return new HashMap<>();
 	}
 	
 	public static Map<String, Long> processXpGains(EventType type, ResourceLocation entityID) {
@@ -62,6 +34,7 @@ public class AutoEntity {
 			return new HashMap<>();
 				
 		Map<String, Long> outMap = new HashMap<>();
+		EntityType<?> entityType = ForgeRegistries.ENTITY_TYPES.getValue(entityID);
 		switch (type) {		 				
 		case FROM_PLAYERS:					 		
 		case MELEE_TO_PLAYERS:						
@@ -72,7 +45,7 @@ public class AutoEntity {
 			break;
 		}
 		case RIDING: {
-			if (ForgeRegistries.ENTITY_TYPES.getValue(entityID).is(Reference.RIDEABLE_TAG)) {
+			if (entityType.is(Reference.RIDEABLE_TAG)) {
 				outMap.putAll(getXpMap(entityID, type));
 			}
 			break;
@@ -85,13 +58,13 @@ public class AutoEntity {
 			break;
 		}
 		case BREED: {
-			if (ForgeRegistries.ENTITY_TYPES.getValue(entityID).is(Reference.BREEDABLE_TAG)) {
+			if (entityType.is(Reference.BREEDABLE_TAG)) {
 				outMap.putAll(getXpMap(entityID, type));
 			}
 			break;
 		}
 		case TAMING: {
-			if (ForgeRegistries.ENTITY_TYPES.getValue(entityID).is(Reference.TAMABLE_TAG)) {
+			if (entityType.is(Reference.TAMABLE_TAG)) {
 				outMap.putAll(getXpMap(entityID, type));
 			}
 			break;
@@ -99,7 +72,10 @@ public class AutoEntity {
 		case FROM_ANIMALS: 
 		case MELEE_TO_ANIMALS: 
 		case RANGED_TO_ANIMALS: {
-			if (ForgeRegistries.ENTITY_TYPES.getValue(entityID).is(Reference.ANIMAL_TAG)) {
+			if (entityType.is(Reference.ANIMAL_TAG)	
+					|| entityType.getCategory() == MobCategory.CREATURE 
+					|| entityType.getCategory() == MobCategory.WATER_CREATURE
+					|| entityType.getCategory() == MobCategory.WATER_AMBIENT) {
 				outMap.putAll(getXpMap(entityID, type));
 			}
 			break;
@@ -107,7 +83,7 @@ public class AutoEntity {
 		case FROM_MOBS: 
 		case MELEE_TO_MOBS:
 		case RANGED_TO_MOBS: {
-			if (ForgeRegistries.ENTITY_TYPES.getValue(entityID).is(Reference.MOB_TAG)) {
+			if (entityType.is(Reference.MOB_TAG) || entityType.getCategory() == MobCategory.MONSTER) {
 				outMap.putAll(getXpMap(entityID, type));
 			}
 			break;
