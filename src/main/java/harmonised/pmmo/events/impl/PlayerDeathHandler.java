1c6b9a37ab4664e68fbbd8b1f156c778e3252b87
Caltinor <62700786+Caltinor@users.noreply.github.com>
2023-07-07 09:22:24 -0400

    refactor death logic.  fixes #317

--- src/main/java/harmonised/pmmo/events/impl/PlayerDeathHandler.java
+++ src/main/java/harmonised/pmmo/events/impl/PlayerDeathHandler.java
@@ -1,7 +1,6 @@
 package harmonised.pmmo.events.impl;
 
 import java.util.HashMap;
-import java.util.Map;
 
 import harmonised.pmmo.config.Config;
 import harmonised.pmmo.core.Core;
@@ -13,35 +12,21 @@ public class PlayerDeathHandler {
 	public static void handle(LivingDeathEvent event) {
 		//we can skip the heavier instanceof calculation if there is no loss on death
 		if (Config.LOSS_ON_DEATH.get() == 0.0) return;
-		double lossRatio = Config.LOSS_ON_DEATH.get() == 1.0 
-				? 1.0
-				: 1 - Config.LOSS_ON_DEATH.get();
-		if (lossRatio <= 0d) return;
 	
 		//IMPORTANT  this is confirmed by the call in EventHandler.  this assumption is critical to this cast
 		Player player = (Player) event.getEntity();
-		Core core = Core.get(player.level);
-		
-		Map<String, Long> currentXp = new HashMap<>(core.getData().getXpMap(player.getUUID()));
-		for (Map.Entry<String, Long> skill :currentXp.entrySet()) {
-			long losableXp = skill.getValue();
-			long finalXp = skill.getValue();
-			if (!Config.LOSE_LEVELS_ON_DEATH.get()) {
-				int currentLevel = core.getData().getLevelFromXP(losableXp);
-				long levelXpThreshold = core.getData().getBaseXpForLevel(currentLevel);
-				if (Config.LOSE_ONLY_EXCESS.get()) {
-					finalXp -= Double.valueOf((losableXp - levelXpThreshold) * lossRatio).longValue();
-				}
-				else {
-					losableXp = Math.min(
-							Double.valueOf(losableXp - levelXpThreshold).longValue(), 
-							Double.valueOf(losableXp * lossRatio).longValue());
-					finalXp -= losableXp;
-				}					
-			}
-			else 
-				finalXp = Double.valueOf(losableXp * lossRatio).longValue();	
-			core.getData().setXpRaw(player.getUUID(), skill.getKey(), finalXp);
-		}
+		Core core = Core.get(player.level());
+
+		new HashMap<>(core.getData().getXpMap(player.getUUID())).forEach((skill, xp) -> {
+			int currentLevel = core.getData().getLevelFromXP(xp);
+			long levelXpThreshold = core.getData().getBaseXpForLevel(currentLevel);
+			long safeXP = Config.LOSE_LEVELS_ON_DEATH.get() ? 0L : levelXpThreshold;
+			long xpToCalculateLoss = Config.LOSE_ONLY_EXCESS.get() ? xp - safeXP : xp;
+
+			long rawLoss = Double.valueOf((double)xpToCalculateLoss * Config.LOSS_ON_DEATH.get()).longValue();
+			long finalXp = Math.max(safeXP, xp - rawLoss);
+
+			core.getData().setXpRaw(player.getUUID(), skill, finalXp);
+		});
 	}
 }
