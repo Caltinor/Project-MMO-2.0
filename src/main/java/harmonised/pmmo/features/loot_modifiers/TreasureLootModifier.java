85ededb9a826f64bf2fa3c1e0ade1888aea0720f
Caltinor <62700786+Caltinor@users.noreply.github.com>
2023-07-07 08:01:27 -0400

    extra drops now drop more than zero. Fixes #315
    an Air itemstack cannot have a count greater than zero, which negated the stack size as a means of storing the count.

--- src/main/java/harmonised/pmmo/features/loot_modifiers/TreasureLootModifier.java
+++ src/main/java/harmonised/pmmo/features/loot_modifiers/TreasureLootModifier.java
@@ -15,6 +15,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.LootParams;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraftforge.common.loot.IGlobalLootModifier;
@@ -25,18 +26,22 @@ public class TreasureLootModifier extends LootModifier{
 	
 	public static final Codec<TreasureLootModifier> CODEC = RecordCodecBuilder.create(instance -> codecStart(instance).and(instance.group(
 			ResourceLocation.CODEC.fieldOf("item").forGetter(tlm -> RegistryUtil.getId(tlm.drop)),
-			Codec.INT.fieldOf("count").forGetter(tlm -> tlm.drop.getCount()),
+			Codec.INT.fieldOf("count").forGetter(tlm -> tlm.count),
 			Codec.DOUBLE.fieldOf("chance").forGetter(tlm -> tlm.chance)
 			)).apply(instance, TreasureLootModifier::new));
 
 	public ItemStack drop;
+	private final int count;
 	public double chance;
 	
 	public TreasureLootModifier(LootItemCondition[] conditionsIn, ResourceLocation lootItemID, int count, double chance) {
 		super(conditionsIn);
 		this.chance = chance;
-		this.drop = new ItemStack(ForgeRegistries.ITEMS.getValue(lootItemID));
+		this.drop = lootItemID.equals(new ResourceLocation("air"))
+				? Items.AIR.getDefaultInstance() 
+				: new ItemStack(ForgeRegistries.ITEMS.getValue(lootItemID));
 		this.drop.setCount(count);
+		this.count = count;
 	}
 
 	@Override
@@ -49,21 +54,26 @@ public class TreasureLootModifier extends LootModifier{
 		if (context.getRandom().nextDouble() <= chance) {
 			
 			//this section checks if the drop is air and replaces it with the block
-			//being broken.  is is the logic for Extra Drops
+			//being broken.  this is the logic for Extra Drops
 			BlockState state = context.getParamOrNull(LootContextParams.BLOCK_STATE);
 			if (state != null && drop.getItem() == Items.AIR) {
-				int count = drop.getCount();
-				drop = new ItemStack(state.getBlock().asItem());
+				drop = state.getDrops(builderFromContext(context)).get(0);
 				drop.setCount(count);
 			}
 			
 			//Notify player that their skill awarded them an extra drop.
 			Entity breaker = context.getParamOrNull(LootContextParams.THIS_ENTITY);
-			if (breaker != null && breaker instanceof Player) {
-				((Player)breaker).sendSystemMessage(LangProvider.FOUND_TREASURE.asComponent());
+			if (breaker instanceof Player player) {
+				player.sendSystemMessage(LangProvider.FOUND_TREASURE.asComponent());
 			}
 			generatedLoot.add(drop.copy());
 		}
 		return generatedLoot;
+	}
+
+	private LootParams.Builder builderFromContext(LootContext context) {
+		return new LootParams.Builder(context.getLevel())
+				.withParameter(LootContextParams.ORIGIN, context.getParam(LootContextParams.ORIGIN))
+				.withParameter(LootContextParams.TOOL, context.getParam(LootContextParams.TOOL));
 	}
 }
