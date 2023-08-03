62eeb052642d8fbd09620be84a83f2fc3a35be1f
Caltinor <62700786+Caltinor@users.noreply.github.com>
2023-08-03 06:29:23 -0400

    refector CONSUME event.  Fixes #337
    This refactor removes the `EatFoodEvent` in favor of an edible and potion check under the `LivingEntityUseItemEvent.Finish` event.  This should produce more consistent behavior and removes the need for two mixins.

--- src/main/java/harmonised/pmmo/events/impl/FoodEatHandler.java
+++ src/main/java/harmonised/pmmo/events/impl/FoodEatHandler.java
@@ -4,28 +4,36 @@ import java.util.List;
 import java.util.Map;
 
 import harmonised.pmmo.api.enums.EventType;
-import harmonised.pmmo.api.events.EatFoodEvent;
 import harmonised.pmmo.core.Core;
 import harmonised.pmmo.features.party.PartyUtils;
 import harmonised.pmmo.util.TagUtils;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.PotionItem;
+import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
+import net.minecraftforge.eventbus.api.Event;
 
 public class FoodEatHandler {
 
-	@SuppressWarnings("resource")
-	public static void handle(EatFoodEvent event) {
-		Core core = Core.get(event.getEntity().level());
+	private static void internalHandle(Event event, Player player, ItemStack food) {
+		Core core = Core.get(player.getLevel());
 		CompoundTag eventHookOutput = new CompoundTag();
-		boolean serverSide = !event.getEntity().level().isClientSide; 
-		if (serverSide)		
+		boolean serverSide = !player.level.isClientSide;
+		if (serverSide)
 			eventHookOutput = core.getEventTriggerRegistry().executeEventListeners(EventType.CONSUME, event, new CompoundTag());
 		//Process perks
-		CompoundTag perkOutput = TagUtils.mergeTags(eventHookOutput, core.getPerkRegistry().executePerk(EventType.CONSUME, event.getEntity(), eventHookOutput));
+		CompoundTag perkOutput = TagUtils.mergeTags(eventHookOutput, core.getPerkRegistry().executePerk(EventType.CONSUME, player, eventHookOutput));
 		if (serverSide) {
-			Map<String, Long> xpAward = core.getExperienceAwards(EventType.CONSUME, event.getFood(), event.getEntity(), perkOutput);
-			List<ServerPlayer> partyMembersInRange = PartyUtils.getPartyMembersInRange((ServerPlayer) event.getEntity());
-			core.awardXP(partyMembersInRange, xpAward);	
+			Map<String, Long> xpAward = core.getExperienceAwards(EventType.CONSUME, food, player, perkOutput);
+			List<ServerPlayer> partyMembersInRange = PartyUtils.getPartyMembersInRange((ServerPlayer) player);
+			core.awardXP(partyMembersInRange, xpAward);
+		}
+	}
+	public static void handle(LivingEntityUseItemEvent.Finish event) {
+		if (event.getEntity() instanceof Player player && (event.getItem().isEdible() || event.getItem().getItem() instanceof PotionItem)) {
+			internalHandle(event, player, event.getItem());
 		}
 	}
 }
