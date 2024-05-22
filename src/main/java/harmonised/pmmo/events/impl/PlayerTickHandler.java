c7881054850c91342f51f6d912d5adf900f37bfc
Caltinor <62700786+Caltinor@users.noreply.github.com>
2024-05-22 17:07:07 -0400

    Fixes 5+ player feature breaks
    Fixes #431 and #432

--- src/main/java/harmonised/pmmo/events/impl/PlayerTickHandler.java
+++ src/main/java/harmonised/pmmo/events/impl/PlayerTickHandler.java
@@ -30,11 +30,12 @@ import net.minecraftforge.fml.LogicalSide;
 public class PlayerTickHandler {
 	private static final Map<UUID, Integer> airLast = new HashMap<>();
 	private static final Map<UUID, Float> healthLast = new HashMap<>();
-	private static short ticksIgnoredSinceLastProcess = 0;
+	private static Map<Player, Short> ticksIgnoredSinceLastProcess = new HashMap<>();
 
 	public static void handle(PlayerTickEvent event) {
-		ticksIgnoredSinceLastProcess++;
-		if (ticksIgnoredSinceLastProcess < 10) return;
+		short current = ticksIgnoredSinceLastProcess.computeIfAbsent(event.player, p -> (short) 0);
+		ticksIgnoredSinceLastProcess.put(event.player, ++current);
+		if (current < 10) return;
 
 		Player player = event.player;
 		Core core = Core.get(event.side);
@@ -88,7 +89,7 @@ public class PlayerTickHandler {
 		//update tracker variables
 		airLast.put(player.getUUID(), player.getAirSupply());
 		healthLast.put(player.getUUID(), player.getHealth());
-		ticksIgnoredSinceLastProcess = 0;
+		ticksIgnoredSinceLastProcess.put(event.player, (short) 0);
 	}
 	
 	private static void processEvent(EventType type, Core core, PlayerTickEvent event) {
