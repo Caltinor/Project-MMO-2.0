ea3cca90757893a22fbd7d931c8523a4d7384330
Caltinor <62700786+Caltinor@users.noreply.github.com>
2024-08-14 07:26:16 -0400

    reimplement CT compat

--- src/main/java/harmonised/pmmo/compat/crafttweaker/CTPerkFunction.java
+++ src/main/java/harmonised/pmmo/compat/crafttweaker/CTPerkFunction.java
@@ -1,29 +1,29 @@
 package harmonised.pmmo.compat.crafttweaker;
 
-//import org.openzen.zencode.java.ZenCodeType;
-//
-//import com.blamejared.crafttweaker.api.annotation.ZenRegister;
-//import com.blamejared.crafttweaker.api.data.MapData;
-//import com.blamejared.crafttweaker_annotations.annotations.Document;
-//
-//import net.minecraft.world.entity.player.Player;
-//
-//@FunctionalInterface
-//@ZenRegister
-//@ZenCodeType.Name("mods.pmmo.CTPerkFunction")
-//@Document("mods/PMMO/CTPerkFunction")
+import org.openzen.zencode.java.ZenCodeType;
+
+import com.blamejared.crafttweaker.api.annotation.ZenRegister;
+import com.blamejared.crafttweaker.api.data.MapData;
+import com.blamejared.crafttweaker_annotations.annotations.Document;
+
+import net.minecraft.world.entity.player.Player;
+
+@FunctionalInterface
+@ZenRegister
+@ZenCodeType.Name("mods.pmmo.CTPerkFunction")
+@Document("mods/PMMO/CTPerkFunction")
 public interface CTPerkFunction {
-//	/**This function represents the logic being executed
-//	 * for a perk.  All perks are provided the player this
-//	 * perk is contextually applicable to, a map of settings
-//	 * and the players current level in the skill associated
-//	 * in the users' config.
-//	 *
-//	 * @param player perk executor
-//	 * @param nbt data provided by PMMO
-//	 * @return output data. not all events use output data, and your perk may not
-//	 * have anything to return.  In this case an empty map is sufficient.
-//	 */
-//	@ZenCodeType.Method
-//	MapData apply(Player player, MapData nbt);
+	/**This function represents the logic being executed
+	 * for a perk.  All perks are provided the player this
+	 * perk is contextually applicable to, a map of settings
+	 * and the players current level in the skill associated
+	 * in the users' config.
+	 *
+	 * @param player perk executor
+	 * @param nbt data provided by PMMO
+	 * @return output data. not all events use output data, and your perk may not
+	 * have anything to return.  In this case an empty map is sufficient.
+	 */
+	@ZenCodeType.Method
+	MapData apply(Player player, MapData nbt);
 }
--- src/main/java/harmonised/pmmo/compat/crafttweaker/CTPerkPredicate.java
+++ src/main/java/harmonised/pmmo/compat/crafttweaker/CTPerkPredicate.java
@@ -1,30 +1,29 @@
 package harmonised.pmmo.compat.crafttweaker;
 
-//import org.openzen.zencode.java.ZenCodeType;
-//
-//import com.blamejared.crafttweaker.api.annotation.ZenRegister;
-//import com.blamejared.crafttweaker.api.data.MapData;
-//import com.blamejared.crafttweaker_annotations.annotations.Document;
-//
-//import net.minecraft.world.entity.player.Player;
-//
-//@FunctionalInterface
-//@ZenRegister
-//@ZenCodeType.Name("mods.pmmo.CTPerkPredicate")
-//@Document("mods/PMMO/CTPerkPredicate")
+import org.openzen.zencode.java.ZenCodeType;
+
+import com.blamejared.crafttweaker.api.annotation.ZenRegister;
+import com.blamejared.crafttweaker.api.data.MapData;
+import com.blamejared.crafttweaker_annotations.annotations.Document;
+
+import net.minecraft.world.entity.player.Player;
+
+@FunctionalInterface
+@ZenRegister
+@ZenCodeType.Name("mods.pmmo.CTPerkPredicate")
+@Document("mods/PMMO/CTPerkPredicate")
 public interface CTPerkPredicate {
-//	/**PMMO checks certain default conditions such as
-//	 * level requirements, and chance.  If you have
-//	 * other conditions by which this perk should activate,
-//	 * such as a specific dimension, this is where you
-//	 * perform that check.  Returning true indicates the
-//	 * requirements were met.
-//	 *
-//	 * @param player perk executor
-//	 * @param nbt data provided by PMMO
-//	 * @param level the current level of the player in the skill configured
-//	 * @return if the perk is permitted to execute
-//	 */
-//	@ZenCodeType.Method
-//	boolean test(Player player, MapData nbt);
+	/**PMMO checks certain default conditions such as
+	 * level requirements, and chance.  If you have
+	 * other conditions by which this perk should activate,
+	 * such as a specific dimension, this is where you
+	 * perform that check.  Returning true indicates the
+	 * requirements were met.
+	 *
+	 * @param player perk executor
+	 * @param nbt data provided by PMMO
+	 * @return if the perk is permitted to execute
+	 */
+	@ZenCodeType.Method
+	boolean test(Player player, MapData nbt);
 }
