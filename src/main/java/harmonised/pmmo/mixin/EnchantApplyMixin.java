eff67785cdd15081cc098a4e157c6852ee244724
cubiferous <->
2024-12-21 23:56:41 +0100

    Fix EnchantApplyMixin

--- src/main/java/harmonised/pmmo/mixin/EnchantApplyMixin.java
+++ src/main/java/harmonised/pmmo/mixin/EnchantApplyMixin.java
@@ -2,7 +2,8 @@ package harmonised.pmmo.mixin;
 
 import java.util.List;
 
-import org.spongepowered.asm.mixin.Debug;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,24 +11,23 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import harmonised.pmmo.api.events.EnchantEvent;
-import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.EnchantmentMenu;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.EnchantmentInstance;
-import net.minecraft.world.level.Level;
 import net.minecraftforge.common.MinecraftForge;
 
-@Debug
 @Mixin(EnchantmentMenu.class)
-public class EnchantApplyMixin {	
-
+public class EnchantApplyMixin {
 	@Inject(method = "lambda$clickMenuButton$1(Lnet/minecraft/world/item/ItemStack;ILnet/minecraft/world/entity/player/Player;ILnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;)V",
 	        at = @At(
-	        	value = "INVOKE", 
-	        	target = "Lnet/minecraft/world/item/ItemStack;enchant(Lnet/minecraft/world/item/enchantment/Enchantment;I)V"), 
-	        locals = LocalCapture.CAPTURE_FAILSOFT)
-	private void projectmmo$$enchantHandle(ItemStack $$stack, int $$1x, Player player, int $$3x, ItemStack $$4x, Level $$5, BlockPos $$6, CallbackInfo ci, ItemStack stack, List<?> $$8, boolean $$9, int $$11, EnchantmentInstance instance) {
-		MinecraftForge.EVENT_BUS.post(new EnchantEvent(player, stack, instance));
- 	}
+	        	value = "INVOKE",
+	        	target = "Lnet/minecraft/advancements/critereon/EnchantedItemTrigger;trigger(Lnet/minecraft/server/level/ServerPlayer;Lnet/minecraft/world/item/ItemStack;I)V"),
+	        locals = LocalCapture.CAPTURE_FAILHARD)
+	private void postEnchantEvents(ItemStack itemstack, int pId, Player pPlayer, int i, ItemStack itemstack1, Level p_39481_, BlockPos p_39482_, CallbackInfo ci, ItemStack itemstack2, List<EnchantmentInstance> list)
+	{
+		for (EnchantmentInstance enchantment : list) {
+			MinecraftForge.EVENT_BUS.post(new EnchantEvent(pPlayer, itemstack2, enchantment));
+		}
+	}
 }
