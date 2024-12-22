7c49be7d57d4a0458bada419fdaf7e89cb9d54e6
cubiferous <61123648+cubiferous@users.noreply.github.com>
2024-12-22 16:30:20 +0100

    [1.20.1] Mixin fixes (#626)
    * Fix CampfireMixin
    
    * Fix EnchantApplyMixin. Refactor EnchantEvent to contain levels spent instead of enchantment details
    
    * Revert "Fix EnchantApplyMixin. Refactor EnchantEvent to contain levels spent instead of enchantment details"
    
    This reverts commit dff1d53a48a299c877f2427c68c423c423c45214.
    
    * Implement MixinPlugin. Use selective mixin loading for dev/prod environments
    
    * Split Campfire Mixin into Dev and Prod variants
    
    * Improve MixinPlugin - filter prod/dev designation and accept default
    
    * Fix EnchantApplyMixin
    
    * update mixin to remove local capture logic
    
    * fix call side to not fail on dedicated server
    
    ---------
    
    Co-authored-by: dankos <dankos@nope.com>
    Co-authored-by: cubiferous <->
    Co-authored-by: Caltinor <62700786+Caltinor@users.noreply.github.com>

--- src/main/java/harmonised/pmmo/mixin/EnchantApplyMixin.java
+++ src/main/java/harmonised/pmmo/mixin/EnchantApplyMixin.java
@@ -1,33 +1,28 @@
 package harmonised.pmmo.mixin;
 
-import java.util.List;
-
-import org.spongepowered.asm.mixin.Debug;
+import net.minecraft.world.item.enchantment.EnchantmentInstance;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import harmonised.pmmo.api.events.EnchantEvent;
-import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.EnchantmentMenu;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.enchantment.EnchantmentInstance;
-import net.minecraft.world.level.Level;
 import net.minecraftforge.common.MinecraftForge;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Debug
 @Mixin(EnchantmentMenu.class)
-public class EnchantApplyMixin {	
-
-	@Inject(method = "lambda$clickMenuButton$1(Lnet/minecraft/world/item/ItemStack;ILnet/minecraft/world/entity/player/Player;ILnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;)V",
+public class EnchantApplyMixin {
+	@Inject(method = "clickMenuButton(Lnet/minecraft/world/entity/player/Player;I)Z",
 	        at = @At(
 	        	value = "INVOKE", 
-	        	target = "Lnet/minecraft/world/item/ItemStack;enchant(Lnet/minecraft/world/item/enchantment/Enchantment;I)V"), 
-	        locals = LocalCapture.CAPTURE_FAILSOFT)
-	private void projectmmo$$enchantHandle(ItemStack $$stack, int $$1x, Player player, int $$3x, ItemStack $$4x, Level $$5, BlockPos $$6, CallbackInfo ci, ItemStack stack, List<?> $$8, boolean $$9, int $$11, EnchantmentInstance instance) {
-		MinecraftForge.EVENT_BUS.post(new EnchantEvent(player, stack, instance));
+	        	target = "Lnet/minecraft/world/inventory/ContainerLevelAccess;execute(Ljava/util/function/BiConsumer;)V"))
+	private void projectmmo$$enchantHandle(Player player, int pId, CallbackInfoReturnable<Boolean> cir) {
+		EnchantmentMenu menu = ((EnchantmentMenu)(Object)this);
+		ItemStack stack = menu.enchantSlots.getItem(0);
+		for (EnchantmentInstance instance : menu.getEnchantmentList(stack, pId, menu.costs[pId])) {
+			MinecraftForge.EVENT_BUS.post(new EnchantEvent(player, stack, instance));
+		}
  	}
 }
