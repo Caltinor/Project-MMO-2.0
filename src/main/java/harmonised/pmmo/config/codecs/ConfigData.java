05b5fa8c33445dde0942734015ee356c2ce255ee
Caltinor <62700786+Caltinor@users.noreply.github.com>
2025-01-10 05:45:57 -0500

    set foundation for config functions

--- src/main/java/harmonised/pmmo/config/codecs/ConfigData.java
+++ src/main/java/harmonised/pmmo/config/codecs/ConfigData.java
@@ -4,8 +4,10 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import harmonised.pmmo.config.readers.ConfigListener;
 
+import java.util.Map;
+
 public interface ConfigData<T> extends DataSource<T>{
     MapCodec<T> getCodec();
-
     ConfigListener.ServerConfigs getType();
+    ConfigData<T> getFromScripting(String param, Map<String, String> value);
  }
