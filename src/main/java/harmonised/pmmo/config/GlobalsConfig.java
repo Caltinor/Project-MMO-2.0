9b367528dc4af743caf61c0b7c3eeee94fa9d716
Caltinor <62700786+Caltinor@users.noreply.github.com>
2025-01-11 18:23:59 -0500

    add globals parsing operations.

--- src/main/java/harmonised/pmmo/config/GlobalsConfig.java
+++ src/main/java/harmonised/pmmo/config/GlobalsConfig.java
@@ -6,6 +6,7 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 import harmonised.pmmo.config.codecs.ConfigData;
 import harmonised.pmmo.config.readers.ConfigListener;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public record GlobalsConfig(Map<String, String> paths,Map<String, String> constants) implements ConfigData<GlobalsConfig> {
@@ -19,9 +20,23 @@ public record GlobalsConfig(Map<String, String> paths,Map<String, String> consta
 			Map.of("example", "value")
 	);}
 
+	private static final String PATHS = "paths";
+	private static final String CONST = "constants";
+	private static final String KEY = "key";
+	@Override
+	public ConfigData<GlobalsConfig> getFromScripting(String param, Map<String, String> value) {
+		Map<String, String> paths = new HashMap<>(this.paths());
+		Map<String, String> constants = new HashMap<>(this.constants());
+		if (param.equals(PATHS))
+			paths.put(value.getOrDefault(KEY, "missing"), value.getOrDefault("value", "missing"));
+		if (param.equals(CONST))
+			constants.put(value.getOrDefault(KEY, "missing"), value.getOrDefault("value", "missing"));
+		return new GlobalsConfig(paths, constants);
+	}
+
 	public static final MapCodec<GlobalsConfig> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
-			Codec.unboundedMap(Codec.STRING, Codec.STRING).fieldOf("paths").forGetter(GlobalsConfig::paths),
-			Codec.unboundedMap(Codec.STRING, Codec.STRING).fieldOf("constants").forGetter(GlobalsConfig::constants)
+			Codec.unboundedMap(Codec.STRING, Codec.STRING).fieldOf(PATHS).forGetter(GlobalsConfig::paths),
+			Codec.unboundedMap(Codec.STRING, Codec.STRING).fieldOf(CONST).forGetter(GlobalsConfig::constants)
 	).apply(instance, GlobalsConfig::new));
 	@Override
 	public MapCodec<GlobalsConfig> getCodec() {return CODEC;}
