f70525675178e0ddf856d59e06c32439130c937b
Caltinor <62700786+Caltinor@users.noreply.github.com>
2026-01-09 16:34:31 -0500

    fix skillup trigger
    Fixes #749

--- src/main/java/harmonised/pmmo/features/loot_modifiers/SkillUpTrigger.java
+++ src/main/java/harmonised/pmmo/features/loot_modifiers/SkillUpTrigger.java
@@ -2,11 +2,13 @@ package harmonised.pmmo.features.loot_modifiers;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import harmonised.pmmo.core.Core;
 import net.minecraft.advancements.critereon.ContextAwarePredicate;
 import net.minecraft.advancements.critereon.EntityPredicate;
 import net.minecraft.advancements.critereon.MinMaxBounds;
 import net.minecraft.advancements.critereon.SimpleCriterionTrigger;
 import net.minecraft.server.level.ServerPlayer;
+import net.neoforged.fml.LogicalSide;
 
 import java.util.Optional;
 
@@ -18,12 +20,27 @@ public class SkillUpTrigger extends SimpleCriterionTrigger<SkillUpTrigger.Trigge
 		return TriggerInstance.CODEC;
 	}
 	public void trigger(ServerPlayer player) {
-		this.trigger(player, p -> true);
+		this.trigger(player, ti -> {
+			long level = Core.get(LogicalSide.SERVER).getData().getLevel(ti.skill(), player.getUUID());
+			return ti.level.matches(level);
+		});
 	}
-	public record TriggerInstance(Optional<ContextAwarePredicate> player, MinMaxBounds.Ints level) implements SimpleCriterionTrigger.SimpleInstance {
+	public record TriggerInstance(Optional<ContextAwarePredicate> player, Longs level, String skill) implements SimpleCriterionTrigger.SimpleInstance {
 		public static final Codec<TriggerInstance> CODEC = RecordCodecBuilder.create(instance -> instance.group(
 				EntityPredicate.ADVANCEMENT_CODEC.optionalFieldOf("player").forGetter(TriggerInstance::player),
-				MinMaxBounds.Ints.CODEC.optionalFieldOf("level").forGetter(ti -> Optional.of(ti.level()))
-		).apply(instance, (p,l) -> new TriggerInstance(p, l.orElse(MinMaxBounds.Ints.ANY))));
+				Longs.CODEC.optionalFieldOf("level").forGetter(ti -> Optional.of(ti.level())),
+				Codec.STRING.fieldOf("skill").forGetter(TriggerInstance::skill)
+		).apply(instance, (p,l,s) -> new TriggerInstance(p, l.orElse(Longs.ANY), s)));
+	}
+
+	public record Longs(Optional<Long> min, Optional<Long> max) implements MinMaxBounds<Long> {
+		public static final Longs ANY = new Longs(Optional.empty(), Optional.empty());
+		public static Codec<Longs> CODEC = MinMaxBounds.createCodec(Codec.LONG, Longs::new);
+
+		public boolean matches(long value) {
+			return min.orElse(0L) >= value && value <= max.orElse(Long.MAX_VALUE);
+		}
+
+		public static Longs between(long min, long max) {return new Longs(Optional.of(min), Optional.of(max));}
 	}
 }
