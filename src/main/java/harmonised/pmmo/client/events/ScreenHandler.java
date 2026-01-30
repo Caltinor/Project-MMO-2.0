f63e49e4a291ae0b75cb4cf20ca4e67033e47a94
Caltinor <62700786+Caltinor@users.noreply.github.com>
2026-01-29 20:15:25 -0500

    update inv skill list to adapt to other widgets

--- src/main/java/harmonised/pmmo/client/events/ScreenHandler.java
+++ src/main/java/harmonised/pmmo/client/events/ScreenHandler.java
@@ -8,6 +8,7 @@ import harmonised.pmmo.client.gui.glossary.components.parts.PlayerSkillWidget;
 import harmonised.pmmo.config.Config;
 import harmonised.pmmo.util.Reference;
 import net.minecraft.client.gui.components.AbstractWidget;
+import net.minecraft.client.gui.components.events.GuiEventListener;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.InventoryScreen;
 import net.neoforged.api.distmarker.Dist;
@@ -23,7 +24,11 @@ public class ScreenHandler {
         Screen screen = event.getScreen();
 
         if (screen instanceof InventoryScreen) {
-            CollapsingPanel panel = new CollapsingPanel(0, 0, 130, screen.height, false);
+            int y = event.getListenersList().stream()
+                    .filter(listener -> listener.getRectangle().left() < 130)
+                    .map(gel -> gel.getRectangle().bottom())
+                    .max(Integer::compareTo).orElse(0);
+            CollapsingPanel panel = new CollapsingPanel(0, y, 130, screen.height, false);
             DetailScroll scroll = new DetailScroll(0, 0, 103, screen.height) {
                 @Override protected boolean scrollbarVisible() {return false;}
             };
